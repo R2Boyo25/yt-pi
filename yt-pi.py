a32fb45c6de9742f45755d9a055d53c0e5874599
R2boyo25 <r2boyo25@gmail.com>
2020-12-30 16:30:07 -0600

    Version 1.1 ( Settings pushed back to v1.2 )

--- yt-pi.py
+++ yt-pi.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 app.config['DataFolder'] = "/".join(os.path.abspath(__file__).split("/")[:-1]) + "/" + "data"
 
+app.secret_key = os.urandom(24)
+
 def CoSo(version):
     version = str(version)
     
@@ -25,7 +27,7 @@ def returnData(filename):
 @app.route('/videos/')
 def videosList():
     
-    links=['<!--This Page Was Auto Generated-->\n<div align=\"center\">\n<br>\n<input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">\n<br><ul id="myMenu">']
+    links=['<!--This Page Was Auto Generated-->\n<div align=\"center\">\n<br>\n<a href=/ ><img src=/data/home.png height=17px /></a>  <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search.." title="Type in a category">\n<br><br><ul id="myMenu">']
     f = []
     
     for (dirpath, dirnames, filenames) in os.walk(database.Database("config.json").get("videofolder")):
@@ -33,7 +35,7 @@ def videosList():
         break
         
     for thing in f:
-        links.append("\n  <li><a align='center' href='{}'><img src='{}' height=10% width=15% /><br><b>{}</b></a><br></li>".format('/videos/'+thing, '/videos/'+thing+'/thumb',thing))
+        links.append("\n  <li><a align='center' href='{}'><img src='{}' height=12% width=15% /><br><b>{}</b></a><br></li>".format('/videos/'+thing.replace("'", "%27"), '/videos/'+thing.replace("'", "%27")+'/thumb',thing))
         
     links.append('</ul></div>')
     
@@ -66,9 +68,9 @@ def videoPage(video):
         
         for file in files:
             
-            if file.endswith('.mp4'):
+            if file.endswith('.mp4') or file.endswith('.webm'):
                 
-                return render_template("video.html", path='/vidfile/' + video + "/" + file, description=desc.replace("\n", "\n<br>"), title=video)
+                return render_template("video.html", path='/vidfile/' + video.replace("'", "%27") + "/" + file, description=desc.replace("\n", "\n<br>"), title=video)
         
         break
 
@@ -87,6 +89,8 @@ def videoPageThumb(video):
                             file)
                             
         break
+
+    return send_from_directory("data", "eye.png")
 
 @app.route("/vidfile/<folder>/<file>")
 def videourlpagething(folder, file):
@@ -122,13 +126,41 @@ def downloadYtVideo():
         
         return render_template("download.html")
 
-@app.route('/add/upload/')
+@app.route('/add/upload/', methods=['GET', 'POST'])
 def uploadLocalVideo():
-    return CoSo(1.1)
+
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            return "No file part: \n" + str( request.files )
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return "No selected file"
+            flash('No selected file')
+            return redirect(request.url)
+        elif request.form['title'] == '':
+            return "Title is required"
+            flash('Title is required')
+            return redirect(request.url)
+        else:
+            filename = secure_filename(file.filename)
+            os.mkdir(database.Database("config.json").get("videofolder")+"/"+request.form['title'])
+            file.save(os.path.join(database.Database("config.json").get("videofolder")+"/"+request.form['title'], filename))
+            with open(database.Database("config.json").get("videofolder")+"/"+request.form['title']+'/'+request.form['title']+".description", 'w') as file1:
+                file1.write(request.form['nm'])
+            return redirect('/videos/{}'.format(request.form['title']))
+        
+        return "Nothing happened???"
+        
+    else:
+        
+        return render_template("upload.html")
 
 @app.route('/settings/')
 def settingsPage():
-    return CoSo(1.1)
+    return CoSo(1.2)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -138,8 +170,6 @@ def page_not_found(e):
 def bad_requesthandler(e):
     return render_template('400.html', error=e)
 
-@app.errorhandler(403)
-def permissiondeniedhandler(e):
-    return render_template('403.html', error=e)
+if __name__ == "__main__":
 
-app.run(debug=True, host='0.0.0.0', port=database.Database("config.json").get("port"))
+    app.run(debug=True, host='0.0.0.0', port=database.Database("config.json").get("port"))
